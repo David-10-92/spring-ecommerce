a047e81a17ebe4e02066ba9a2876f84f44d77e5b
David Cazalla <ncedcb@outlook.es>
2024-01-29 18:08:15 +0100

    creando los atributos de registro del user y el controlador

---
+++ src/main/java/com/example/springecommerce/api/model/RegistrationBody.java
@@ -0,0 +1,65 @@
+package com.example.springecommerce.api.model;
+
+import jakarta.validation.constraints.*;
+
+public class RegistrationBody {
+    @NotNull
+    @NotBlank
+    @Size(min = 3,max = 255)
+    private String username;
+    @NotNull
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!])(?=\\S+$).{6,}$")
+    @Size(min = 6,max = 32)
+    private String password;
+    @NotNull
+    @NotBlank
+    @Email
+    private String email;
+    @NotNull
+    @NotBlank
+    private String firstName;
+    @NotNull
+    @NotBlank
+    private String lastName;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstname) {
+        this.firstName = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastname) {
+        this.lastName = lastname;
+    }
+}
