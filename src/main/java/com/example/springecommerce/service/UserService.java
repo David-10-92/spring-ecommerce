5e7164465988d09cfbbaaea72183c91fec45cc2a
David Cazalla <ncedcb@outlook.es>
2024-01-31 10:46:06 +0100

    añadir la encriptacion a la contraseña

--- src/main/java/com/example/springecommerce/service/UserService.java
+++ src/main/java/com/example/springecommerce/service/UserService.java
@@ -1,18 +1,25 @@
 package com.example.springecommerce.service;
 
+import com.example.springecommerce.api.model.LoginBody;
 import com.example.springecommerce.api.model.RegistrationBody;
 import com.example.springecommerce.exception.UserAlreadyExistsException;
 import com.example.springecommerce.model.LocalUser;
 import com.example.springecommerce.model.dao.LocalUserDAO;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
 
     private LocalUserDAO localUserDAO;
+    private EncryptionService encryptionService;
+    private JWTService jwtService;
 
-    public UserService(LocalUserDAO localUserDAO) {
+    public UserService(LocalUserDAO localUserDAO, EncryptionService encryptionService, JWTService jwtService) {
         this.localUserDAO = localUserDAO;
+        this.encryptionService = encryptionService;
+        this.jwtService = jwtService;
     }
 
     public LocalUser registerUser(RegistrationBody registrationBody) throws UserAlreadyExistsException {
@@ -26,7 +33,18 @@ public class UserService {
         user.setLastName(registrationBody.getLastName());
         user.setUsername((registrationBody.getUsername()));
         //Encrypt passwords!!
-        user.setPassword(registrationBody.getPassword());
+        user.setPassword(encryptionService.encryptPassword(registrationBody.getPassword()));
         return localUserDAO.save(user);
+    }
+
+    public String loginUser(LoginBody loginBody){
+        Optional<LocalUser> opUser = localUserDAO.findByUsernameIgnoreCase(loginBody.getUsername());
+        if(opUser.isPresent()){
+            LocalUser user = opUser.get();
+            if(encryptionService.verifyPassword(loginBody.getPassword(),user.getPassword())){
+                return jwtService.generateJWT(user);
+            }
+        }
+        return null;
     }
 }
